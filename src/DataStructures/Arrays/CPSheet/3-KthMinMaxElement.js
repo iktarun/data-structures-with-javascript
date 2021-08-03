798943ef5f5a3affdf223aaea1c7cb4859fcc72b
Tarun Kumar <tarunkumar2215@gmail.com>
2021-08-03 11:04:14 +0530

    CPE sheet cp#4

---
+++ src/DataStructures/Arrays/CPSheet/3-KthMinMaxElement.js
@@ -0,0 +1,12 @@
+// UsingMinHeap
+
+function kthSmallestElement(arr, k) {}
+
+function kthLargestElement(arr, k) {}
+
+let arr = [7, 10, 4, 3, 20, 15];
+let k = 3;
+
+console.log(kthSmallestElement(arr, k));
+
+console.log(kthLargestElement(arr, k));
---
+++ src/DataStructures/Arrays/CPSheet/4-sort0s1s2s.js
@@ -0,0 +1,26 @@
+function sortArray(arr) {
+  let mid = 0;
+  let low = 0;
+  let high = arr.length - 1;
+  let temp;
+  while (mid <= high) {
+    if (arr[mid] === 0) {
+      temp = arr[mid];
+      arr[mid] = arr[low];
+      arr[low] = temp;
+      low++;
+      mid++;
+    } else if (arr[mid] === 1) {
+      mid++;
+    } else if (arr[mid] === 2) {
+      temp = arr[mid];
+      arr[mid] = arr[high];
+      arr[high] = temp;
+      high--;
+    }
+  }
+  return arr;
+}
+
+let arr = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1];
+console.log(sortArray(arr));
