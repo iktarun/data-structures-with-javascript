a99494f50535edb3c184f46b62435457d1ead254
Tarun Kumar <tarunkumar2215@gmail.com>
2021-08-05 15:47:32 +0530

    Handled a case for the input
    Input
    [1,2,2]
    Output
    -1
    Expected
    2

--- src/DataStructures/Arrays/CPSheet/9-DuplicateInArray.js
+++ src/DataStructures/Arrays/CPSheet/9-DuplicateInArray.js
@@ -7,7 +7,16 @@ var findDuplicate = function (nums) {
     if (nums[Math.abs(item)] < 0) {
       return Math.abs(item);
     } else {
-      nums[item] *= -1;
+      /** case: If we dont use Math.abs(item) in the below, it can cause the trouble
+         * Input
+            [1,2,2]
+            Output
+            -1
+            Expected
+            2
+             nums[item] *= -1;
+         */
+      nums[Math.abs(item)] *= -1;
     }
   }
   return -1;
