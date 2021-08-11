78f914f3790b06c17a3bc9adae15a3f7b3d9190a
Tarun Kumar <tarunkumar2215@gmail.com>
2021-08-11 09:36:31 +0530

    #13: Kadanes Algorithm

--- src/DataStructures/Arrays/CPSheet/8-maxSubArraySum.js
+++ src/DataStructures/Arrays/CPSheet/8-maxSubArraySum.js
@@ -18,6 +18,25 @@ function maxSubarraySum(arr) {
   }
   return maxSum;
 }
+/**
+ * O(n)
+ * @param {*} arr
+ * @returns arr[]
+ */
+function maxSubarraySumUsingKadanesAlgorithm(arr) {
+  let meh = 0;
+  let msf = Number.MIN_SAFE_INTEGER;
+  for (let i = 0; i < arr.length; i++) {
+    meh = meh + arr[i];
+    if (meh < arr[i]) {
+      meh = arr[i];
+    }
+    if (msf < meh) {
+      msf = meh;
+    }
+  }
+  return msf;
+}
 
-let arr = [-1, -2, -3, -4];
-console.log(maxSubarraySum(arr));
+let arr = [-2, -3, 4, -1, -2, 1, 5, -3];
+console.log(maxSubarraySumUsingKadanesAlgorithm(arr));
