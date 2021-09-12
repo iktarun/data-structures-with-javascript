ea456c8ebd9b4e7249da1da94a3b323a7eb86987
Tarun Kumar <tarunkumar2215@gmail.com>
2021-09-12 08:08:03 +0530

    Removed consoles

--- src/DataStructures/CPSheet/Strings/56-SubsequencesOfString.js
+++ src/DataStructures/CPSheet/Strings/56-SubsequencesOfString.js
@@ -1,12 +1,9 @@
 function subSequences(input, output) {
-  console.log("1st:", input, output);
   if (input.length === 0) {
-    console.log("output<<", output);
     return;
   }
 
   subSequences(input.substr(1), output);
-  console.log("2nd:", input, output);
 
   subSequences(input.substr(1), output + input.charAt(0));
 }
