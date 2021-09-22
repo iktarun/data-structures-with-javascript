46a220a31150569cd0986b2b1495126993cb1e17
Tarun Kumar <tarunkumar2215@gmail.com>
2021-09-22 09:04:49 +0530

    Train Arrival and Departure problem

---
+++ src/DataStructures/Random/TrainArrivalDepartureProblem.js
@@ -0,0 +1,39 @@
+//railway station such that no train waits.
+findPlatform(arr, dep, n);
+{
+  arr.sort();
+  dep.sort();
+
+  // plat_needed indicates
+  // number of platforms
+  // needed at a time
+  let plat_needed = 1;
+  let result = 1;
+  let i = 1;
+  let j = 0;
+
+  // Similar to merge in
+  // merge sort to process
+  // all events in sorted order
+  while (i < n && j < n) {
+    // If next event in sorted
+    // order is arrival, increment
+    // count of platforms needed
+    if (arr[i] <= dep[j]) {
+      plat_needed++;
+      i++;
+    }
+
+    // Else decrement count
+    // of platforms needed
+    else if (arr[i] > dep[j]) {
+      plat_needed--;
+      j++;
+    }
+
+    // Update result if needed
+    if (plat_needed > result) result = plat_needed;
+  }
+
+  return result;
+}
