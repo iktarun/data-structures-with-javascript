d21303da6abd6883e5935572965990ae7a95960c
Tarun Kumar <tarunkumar2215@gmail.com>
2022-05-06 19:25:29 +0530

    name changes of function

--- src/DataStructures/Bits/NonRepeatNoAllOccuredThreeTimes.js
+++ src/DataStructures/Bits/NonRepeatNoAllOccuredThreeTimes.js
@@ -1,19 +1,20 @@
 //Finding an element in an array that isn't repeated a multiple of three times?
 
-function countSetBits(numbers) {
+function findNo(numbers) {
   let occured = new Array(32).fill(0);
+
+  //Take an array of 32 places and keep the count of the set location inside that
   for (let j = 0; j < numbers.length; j++) {
     let k = 0;
     let n = numbers[j];
     while (n > 0) {
-      occured[k] += n & 1;
+      occured[k++] += n & 1;
       n = n >> 1;
-      k += 1;
     }
     // console.log(j, k, n, occured);
   }
 
-  //Take modulo with 3 to find the number in binary
+  //Take modulo with 3 to find the number in binary and set bit of no is not multiple of 3
   for (let j = 0; j < occured.length; j++) {
     if (occured[j] % 3 !== 0) {
       occured[j] = 1;
@@ -34,4 +35,4 @@ function countSetBits(numbers) {
 
 // driver program
 let numbers = [9, 2, 2, 2, 3, 3, 3];
-console.log("No is:" + countSetBits(numbers));
+console.log("No is:" + findNo(numbers));
