88934b946c360551c03013140e81fbf44bec5efa
Tarun Kumar <tarunkumar2215@gmail.com>
2022-06-03 10:42:05 +0530

    Done some change to counter app

--- src/BasicsJSConcepts/problems/StartPauseStopCounterApp.js
+++ src/BasicsJSConcepts/problems/StartPauseStopCounterApp.js
@@ -1,18 +1,23 @@
 let ladder = {
-  step: 1,
+  step: 0,
   intervalId: null,
   start() {
     this.intervalId = setInterval(() => {
-      console.log(this.step++);
+      console.log(++this.step);
     }, 1000);
   },
   pause() {
-    clearInterval(this.intervalId);
-    console.log(this.step);
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      //   console.log(this.step);
+    }
   },
   stop: function () {
     // shows the current step
-    this.step = 1;
-    console.log(this.step);
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.step = 0;
+      return;
+    }
   },
 };
