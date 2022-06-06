9932b4cfc42356f4c5fec4f6f9375fbaf39de891
Tarun Kumar <tarunkumar2215@gmail.com>
2022-06-06 22:11:20 +0530

    Generators update

--- src/BasicsJSConcepts/Generators.js
+++ src/BasicsJSConcepts/Generators.js
@@ -1,20 +1,41 @@
 function* ayncCall() {
+  yield 1000;
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      return resolve(100);
+      resolve(100);
     });
   });
 }
 
+// The function* declaration (function keyword followed by an asterisk) defines a generator function, which returns a Generator object.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function*
 function* generator(i) {
-  yield* ayncCall();
+  // The yield* expression is used to delegate to another generator or iterable object.
+  yield* ayncCall(); //Will not wait
   yield i + 10;
 }
 
 const gen = generator(10);
 
-console.log(gen.next().value);
+console.log("1st output:", gen.next().value);
 // expected output: 10
 
-console.log(gen.next().value);
+console.log("2nd output:", gen.next().value);
 // expected output: 20
+
+/**
+ * Simple Example
+ 
+
+function* generateSequence() {
+  yield 1;
+  yield 2;
+  return 3;
+}
+
+let generator = generateSequence();
+
+console.log(JSON.stringify(generator.next())); // {value: 1, done: false}
+console.log(JSON.stringify(generator.next())); // {value: 1, done: false}
+console.log(JSON.stringify(generator.next())); // {value: 1, done: false}
+*/
