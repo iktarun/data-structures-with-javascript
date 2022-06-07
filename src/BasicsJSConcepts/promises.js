8f3c5cb0c782f704e428fdaed2f5c586a247ff0c
Tarun Kumar <tarunkumar2215@gmail.com>
2022-06-07 10:16:59 +0530

    Promise chain

--- src/BasicsJSConcepts/promises.js
+++ src/BasicsJSConcepts/promises.js
@@ -11,29 +11,28 @@
 
 ///*
 new Promise(function (resolve, reject) {
-  // try {
+  //\ try {
   return setTimeout(() => {
-    // throw new Error("Whoops!");
+    throw new Error("Whoops!");
     resolve("Success");
-  }, 1000);
+  }, 3000);
   // } catch (err) {
   //   console.log("ERROR:1", err);
   // }
 })
   .then(
     (result, error) => {
+      console.log("Inside chain", result);
       throw new Error("Whoops!");
-      console.log("Inside chain");
+    },
+    (error) => {
+      console.log("Inside error");
     }
-    // ,
-    // (error) => {
-    //   console.log("Inside error");
-    // }
   )
   .then(
     (result, error) => {
-      throw new Error("Whoops2!");
       console.log("Inside chain2");
+      // throw new Error("Whoops2!");
     },
     (error) => {
       console.log("Inside error2");
