6926481baaca21c5479b2eb04503f772319c2945
Tarun Kumar <tarunkumar2215@gmail.com>
2022-07-04 16:30:31 +0530

    Some updateds

--- src/DataStructures/CPSheet/Stack/275-2StacksInAnArray.js
+++ src/DataStructures/CPSheet/Stack/275-2StacksInAnArray.js
@@ -6,10 +6,10 @@
 
 class TwoStacks {
   constructor() {
-    this.arr = new Array(5);
-    this.size = 5;
+    this.arr = new Array(100);
+    this.size = 100;
     this.top1 = -1;
-    this.top2 = 5;
+    this.top2 = 100;
   }
 
   /**
@@ -17,53 +17,42 @@ class TwoStacks {
    */
   //Function to push an integer into the stack1.
   push1(x) {
-    // code here
-    if (this.top1 + 1 > this.top2) {
-      console.log("Stack full", this.top1, this.top2);
-      return;
+    // There is at least one empty space for
+    // new element
+    if (this.top1 < this.top2 - 1) {
+      this.top1++;
+      this.arr[this.top1] = x;
     }
-    this.arr[++this.top1] = x;
   }
 
-  /**
-   * @param {number} x
-   */
-  //Function to push an integer into the stack2.
+  // Method to push an element x to stack2
   push2(x) {
-    // code here
-    if (this.top2 - 1 <= this.top1) {
-      console.log("Stack full", this.top1, this.top2);
-      return;
+    // There is at least one empty space for
+    // new element
+    if (this.top1 < this.top2 - 1) {
+      this.top2--;
+      this.arr[this.top2] = x;
     }
-    this.arr[--this.top2] = x;
   }
 
-  /**
-   * @returns {number}
-   */
-  //Function to remove an element from top of the stack1.
+  // Method to pop an element from first stack
   pop1() {
-    // code here
-    if (this.top < 0) {
-      return;
+    if (this.top1 >= 0) {
+      var x = this.arr[this.top1];
+      this.top1--;
+      return x;
     }
-    let item = this.arr[this.top1];
-    this.arr[this.top1--] = undefined;
-    return item;
+    return -1;
   }
 
-  /**
-   * @returns {number}
-   */
-  //Function to remove an element from top of the stack2.
+  // Method to pop an element from second stack
   pop2() {
-    // code here
-    if (this.top2 > this.size) {
-      return;
+    if (this.top2 < this.size) {
+      var x = this.arr[this.top2];
+      this.top2++;
+      return x;
     }
-    let item = this.arr[this.top2];
-    this.arr[this.top2++] = undefined;
-    return item;
+    return -1;
   }
 }
 
