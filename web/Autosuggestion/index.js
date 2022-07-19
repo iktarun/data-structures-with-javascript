763e57051c0415de34ecc9b4810a5710bbbf6888
Tarun Kumar <tarunkumar2215@gmail.com>
2022-07-19 17:54:40 +0530

    Minor changes

--- web/Autosuggestion/index.js
+++ web/Autosuggestion/index.js
@@ -1,17 +1,18 @@
 let items = ["apple", "applie", "apac", "application"];
 
-let suggestion = document.getElementById("suggestions");
-
 function clearSuggestions() {
+  let suggestion = document.getElementById("suggestions");
   suggestion.innerHTML = "";
 }
 
 function debounce(func, delay) {
   let timer;
-  return function (...args) {
+  return function () {
+    let self = this;
+    let args = arguments;
     clearTimeout(timer);
     timer = setTimeout(function () {
-      func.apply(this, args);
+      func.apply(self, args);
     }, delay);
   };
 }
@@ -31,7 +32,7 @@ function onSearch(e) {
   let ul = document.createElement("ul");
 
   let suggestion = document.getElementById("suggestions");
-  suggestion.innerHTML = "";
+  clearSuggestions();
 
   for (let i = 0; i < filtered.length; i++) {
     let newLiTag = document.createElement("li");
